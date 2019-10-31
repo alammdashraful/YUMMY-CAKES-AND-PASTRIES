da6092b799bd2d356e2bc45de101d7477486892f
ALAM MD ASHRAFUL <ashraful@qq.com>
2019-10-31 13:42:23 +0800

    update product details and customer reviews

---
+++ images/script.js
@@ -0,0 +1,13 @@
+/*jslint plusplus: true */
+/*global $, jQuery, console, alert, prompt */
+
+$('.carousel').carousel();
+
+// Em An
+// 13-7-2017
+// https://codepen.io/collection/AGNJNB/
+// https://codepen.io/Dr_rakcha/pen/WOWrxr
+// https://codepen.io/Em-An/pen/NbQZjZ
+
+// https://codepen.io/mephysto/pen/ZYVKRY
+
